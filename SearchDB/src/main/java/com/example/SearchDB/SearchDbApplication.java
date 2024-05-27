f11aeaa0d93f3b73bd25b27097d442616a053580
AndreiShev <shevelyov.andreas@gmail.com>
2024-05-27 23:43:20 +0600

    Инициализация проекта

---
+++ SearchDB/src/main/java/com/example/SearchDB/SearchDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.SearchDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchDbApplication.class, args);
+	}
+
+}
